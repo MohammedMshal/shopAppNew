416b4c8e96058d586bebca6198dc8907a66caab5
MohammedMshal <mohammedmshal2018@gmail.com>
2022-06-28 17:22:54 +0200

    Network Hleper

---
+++ lib/shared/network/remote/dio_helper.dart
@@ -0,0 +1,89 @@
+import 'package:dio/dio.dart';
+
+class DioHelper {
+  static late Dio dio;
+
+  static int() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://student.valuxapps.com/api/',
+        receiveDataWhenStatusError: true,
+      ),
+    );
+  }
+
+  static Future<Response> getData({
+    required String url,
+    Map<String, dynamic>? query,
+    String lang = 'en',
+    String? token,
+  }) async {
+    dio.options.headers = {
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? ''
+    };
+    return dio.get(
+      url,
+      queryParameters: query,
+    );
+  }
+
+  static Future<Response> postData({
+    required String url,
+    required Map<String, dynamic> data,
+    Map<String, dynamic>? query,
+    String lang = 'en',
+    String? token,
+  }) async {
+    dio.options.headers = {
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? ''
+    };
+    return dio.post(
+      url,
+      queryParameters: query,
+      data: data,
+    );
+  }
+
+  static Future<Response> putData({
+    required String url,
+    required Map<String, dynamic>? data,
+    Map<String, dynamic>? query,
+    String lang = 'en',
+    String? token,
+  }) async {
+    dio.options.headers = {
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? ''
+    };
+    return dio.put(
+      url,
+      queryParameters: query,
+      data: data,
+    );
+  }
+
+  static Future<Response> deleteData({
+    required String url,
+    required Map<String, dynamic>? data,
+    Map<String, dynamic>? query,
+    String lang = 'en',
+    String? token,
+  }) async {
+    dio.options.headers = {
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? ''
+    };
+    return dio.delete(
+      url,
+      queryParameters: query,
+      data: data,
+    );
+  }
+
+}
